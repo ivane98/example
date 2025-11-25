defcd6be6ba3147e292087a0efd24222325050f5
ivane98 <i.vardoshvili22@gmail.com>
2025-11-25 08:42:09 +0400

    another test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
-//another one
+
 // Route::resource('jobs', JobController::class);
 
 Route::controller(JobController::class)->group(function () {
